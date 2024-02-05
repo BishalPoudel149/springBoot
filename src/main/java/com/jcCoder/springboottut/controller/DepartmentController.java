2a7fe85fea33d00f81cb6f02120ed826a916721c
I528624 <bishal.poudel@sap.com>
2024-02-06 00:10:29 +0530

    Post Method Started

--- src/main/java/com/jcCoder/springboottut/controller/DepartmentController.java
+++ src/main/java/com/jcCoder/springboottut/controller/DepartmentController.java
@@ -1,7 +1,14 @@
 package com.jcCoder.springboottut.controller;
 
+import com.jcCoder.springboottut.entity.Department;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DepartmentController {
+    @PostMapping("/department")
+    public void saveDepartment( @RequestBody Department department){
+
+    }
 }
