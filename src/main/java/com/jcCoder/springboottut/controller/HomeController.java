b8bc36449426860f9306ba5019761bd632abcb1e
I528624 <bishal.poudel@sap.com>
2024-02-05 22:30:27 +0530

    Initial Commit

---
+++ src/main/java/com/jcCoder/springboottut/controller/HomeController.java
@@ -0,0 +1,8 @@
+package com.jcCoder.springboottut.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+}
---
+++ src/test/java/com/jcCoder/springboottut/SpringBootTutApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jcCoder.springboottut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
