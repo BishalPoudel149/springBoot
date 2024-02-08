223d3594a9ed8ab822b4229502c98ee0ce0fa478
BishalPoudel149 <bishalpoudel563@gmail.com>
2024-02-09 00:02:07 +0530

    Deletion, Get, Put & Hibernate-Validation , Logger

--- src/main/java/com/jcCoder/springboottut/entity/Department.java
+++ src/main/java/com/jcCoder/springboottut/entity/Department.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Department {
@@ -11,26 +14,34 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 
-    private  Long departmentId;
+    private Long departmentId;
+
+    @NotBlank(message = "Enter the Department name")
     private String departmentName;
 
-    private  String departmentAddress;
+    @NotNull(message = "Address Can't be null")
+    private String departmentAddress;
 
-    private  String departmentCode;
+    private String departmentCode;
 
+    @Email(message = "Invalid Email Address , Please Enter a valid mail")
+    private String emailAddress;
 
     public Department() {
+
     }
-    public Department(Long departmentId, String departmentName, String departmentAddress) {
+
+
+    public Department(Long departmentId, String departmentName, String departmentAddress, String emailAddress) {
         this.departmentId = departmentId;
         this.departmentName = departmentName;
         this.departmentAddress = departmentAddress;
+        this.emailAddress = emailAddress;
     }
 
     public Long getDepartmentId() {
         return departmentId;
     }
-
 
 
     public void setDepartmentId(Long departmentId) {
@@ -52,6 +63,7 @@ public class Department {
     public void setDepartmentAddress(String departmentAddress) {
         this.departmentAddress = departmentAddress;
     }
+
     public String getDepartmentCode() {
         return departmentCode;
     }
@@ -60,14 +72,17 @@ public class Department {
         this.departmentCode = departmentCode;
     }
 
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
 
     @Override
     public String toString() {
-        return "Department{" +
-                "departmentId=" + departmentId +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentAddress='" + departmentAddress + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
+        return "Department{" + "departmentId=" + departmentId + ", departmentName='" + departmentName + '\'' + ", departmentAddress='" + departmentAddress + '\'' + ", departmentCode='" + departmentCode + '\'' + ", emailAddress='" + emailAddress + '\'' + '}';
     }
 }
--- src/main/java/com/jcCoder/springboottut/repository/DepartmentRepository.java
+++ src/main/java/com/jcCoder/springboottut/repository/DepartmentRepository.java
@@ -1,9 +1,17 @@
 package com.jcCoder.springboottut.repository;
 
 import com.jcCoder.springboottut.entity.Department;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Transactional
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    public  Department findByDepartmentNameIgnoreCase(String departmentName);
+
+    public List<Department> findByDepartmentAddress(String address);
 }
